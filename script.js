15bfa2b8fcbb83c7d27ecb8c145ce0a08f55d302
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-01 10:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelector('#sizeInfo');
+const sizeHeader = sizeInfo.querySelector('h1');
+
+function updateSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  sizeHeader.textContent = `Width: ${width} Height: ${height}`;
+}
+
+// Call updateSize() on page load to initialize the header with the current size
+updateSize();
+
+// Call updateSize() whenever the window is resized
+window.addEventListener('resize', updateSize);
